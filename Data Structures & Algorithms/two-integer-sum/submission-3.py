559f6f5b44dd2777d27b948c0a51b214476298e4
Rohit981 <46937934+Rohit981@users.noreply.github.com>
2026-05-31 14:17:27 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
+                #Optimal Approach
+                hashset = {}
+                #Iterate through indices
+                for i in range(len(nums)):
+                        #Define a difference variable
+                        dif = target - nums[i]
+
+                        if dif in hashset:
+                                return[hashset[dif], i]
+                        
+                        hashset[nums[i]] = i
+            
